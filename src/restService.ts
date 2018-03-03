01d8208aef483909fe87132cc78742ff58b1703c
Aivars Liepa <aiciz@inbox.lv>
2018-03-03 16:11:20 +0200

    restService refactor

--- src/restService.ts
+++ src/restService.ts
@@ -2,22 +2,20 @@ import { Express } from "express-serve-static-core";
 import client from "./twitterClient";
 
 const service = (app: Express) => {
-  app.get("/api/twitter/:q", (req, res) => {
+  app.get("/api/twitter/:q", async (req, res) => {
     const q: string = req.params.q;
 
     if (q.length > 500) {
       return res.status(400).send({ error: "max 500 characters" });
     }
 
-    client
-      .get("search/tweets", { q, count: 10 })
-      .then((tweets: any) => {
-        res.send({ tweets: tweets.statuses });
-      })
-      .catch((error: any) => {
-        console.log(error);
-        res.status(503).send();
-      });
+    try {
+      const tweets: any = await client.get("search/tweets", { q, count: 10 });
+      res.send({ tweets: tweets.statuses });
+    } catch (error) {
+      console.log(error);
+      res.status(503).send();
+    }
   });
 };
 
